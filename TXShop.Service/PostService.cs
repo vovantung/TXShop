77192ed0da95bb2ba7f999b0e119a15be412cc84
VanTung <32097654+vovantung@users.noreply.github.com>
2021-06-18 09:30:20 -0700

    Add1

---
+++ TXShop.Service/PostService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TXShop.Model.Models;
+
+namespace TXShop.Service
+{
+    public interface IPostService
+    {
+        void Add(Post post);
+        void Update(Post post);
+        void Delete(int id);
+        IEnumerable<Post> GetAll();
+        IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow);
+        Post GetById(int id);
+        IEnumerable<Post> GetAllByTagPaging(int page, int pageSize, out int totalRow);
+        void SaveChanges();
+
+
+    }
+    public class PostService : IPostService
+    {
+        public void Add(Post post)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAllByTagPaging(int page, int pageSize, out int totalRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Post> GetAllPaging(int page, int pageSize, out int totalRow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Post GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SaveChanges()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Post post)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
